bd6e352927e61a3ac7d733e2cf2edd25001437a6
mmos <kudo.youtub.men@gmail.com>
2024-12-14 20:56:53 +0700

    lab2 Seletskiy

---
+++ l2.py
@@ -0,0 +1,16 @@
+def main():
+    full_name = "Селецкий Никита Алексеевич"
+    faculty = "Организация и технологии защиты информации (по отрасли или в сфере профессиональной деятельности)"
+    course = "2"
+    group = "ОЗИв-22у"
+    email = "kudo.youtub.men@gmail.com"
+
+    print(f"ФИО: {full_name}")
+    print(f"Факультет: {faculty}")
+    print(f"Курс: {course}")
+    print(f"Группа: {group}")
+    print(f"Email: {email}")
+
+
+if __name__ == "__main__":
+    main()
